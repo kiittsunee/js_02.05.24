a3ad1fe533913f202d39de7abdba74a4a7abed12
Anna <annaocirova5@gmail.com>
2024-04-25 11:12:45 +0800

    Функция  chunk()

---
+++ chunk.js
@@ -0,0 +1,11 @@
+const chunk = (arr, chunkSize) => {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        res += (arr.slice(i, i + chunkSize));
+    }
+
+    return res;
+};
+
+module.exports = {chunk};
+    
