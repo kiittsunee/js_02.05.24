26b25a5f095b9ae9247c2ee49f7f4cfc0763f582
ann <ocirovaanna5@gmail.com>
2024-05-30 00:11:44 +0800

    Изменение compareVersion

--- compareVersion.js
+++ compareVersion.js
@@ -1,13 +1,10 @@
 const compareVersion = (version1,version2) => {
-    var arrValue;
-    if(version1.length > version2.length) {
-        return 1
-    }
-    for (let i=0; i < version1.length; i++) {
-        if(version1[i]!=version2[i]){
-            console.log(version1)
-            console.log(version2)
-            arrValue = version1[i] - version2[i];
+    var arr1 = version1.split('.')
+    var arr2 = version2.split('.')
+    const len = Math.max(arr1.length, arr2.length)
+    for (let i=0; i < len; i++) {
+        if(arr1[i]!=arr2[i]){
+            arrValue = arr1[i] - arr2[i];
             if (arrValue > 0) {
                 return 1
             };
@@ -18,8 +15,6 @@ const compareVersion = (version1,version2) => {
             break;
         };
     };
-    console.log(version1)
-    console.log(version2)
     return 0
 };
 
--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const {compareVersion} = require('./compareVersion.js');
 
 console.log(compareVersion('0.1', '0.2'));
 console.log(compareVersion('0.2', '0.1'));
-console.log(compareVersion('4.2', '4.2'));
+console.log(compareVersion('4.1', '4.2'));
+console.log(compareVersion('1.12', '13.37'));
 
 const arr1 = ['a', 'b', 'c', 'd'];
 console.log(chunk(arr1, 3)); // [['a', 'b', 'c'], ['d']]
